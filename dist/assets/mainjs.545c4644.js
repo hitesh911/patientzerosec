5a79c0c7fff4f27b90647be0180ec77041ac6f93
hitesh911 <72139864+hitesh911@users.noreply.github.com>
2022-02-14 19:25:26 +0300

    may be final :

--- dist/assets/mainjs.545c4644.js
+++ dist/assets/mainjs.545c4644.js
@@ -1,1 +1,12 @@
 
+function open_thanks_modal() {
+    var modal = document.getElementById("thanks-modal");
+    modal.style.display = "block"
+}
+
+function close_thanks_modal() {
+    var modal = document.getElementById("thanks-modal");
+    modal.style.display = "none"
+    window.location.reload(true)
+
+}
