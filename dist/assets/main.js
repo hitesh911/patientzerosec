76300fb2819f7c48f7ef3ba2113f5515fd4174ee
hitesh911 <72139864+hitesh911@users.noreply.github.com>
2022-02-16 10:31:24 +0300

    fjs

---
+++ dist/assets/main.js
@@ -0,0 +1,30 @@
+
+function open_thanks_modal() {
+    var modal = document.getElementById("thanks-modal");
+    modal.style.display = "block"
+}
+
+function close_thanks_modal() {
+    var modal = document.getElementById("thanks-modal");
+    modal.style.display = "none"
+    window.location.reload(true)
+
+}
+
+// dropdown
+console.log("hello")
+document.addEventListener("click" ,e =>{
+    const is_services_dropdown_menu = e.target.matches("[data-dropdown-button]")
+    if(!is_services_dropdown_menu && e.target.closest('[data-dropdown]') != null) return
+    let currentDropdown
+    if(is_services_dropdown_menu){
+        currentDropdown = e.target.closest("[data-dropdown]")
+        currentDropdown.classList.toggle('active1')
+        
+    }
+    document.querySelectorAll("[data-dropdown].active1").forEach(dropdown =>{
+        if(dropdown === currentDropdown) return
+        dropdown.classList.remove("active1")
+    })
+})
+
